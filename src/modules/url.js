c4be97efcdbadcb63bfd009de7447f86fd5cc0cc
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-19 18:44:09 +0300

    Add API URL module

---
+++ src/modules/url.js
@@ -0,0 +1,5 @@
+const url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+//const url = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+const objectId = [1454, 98175, 195133, 188523, 238523, 451941];
+
+export { url, objectId };
