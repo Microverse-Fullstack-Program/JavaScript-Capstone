1d58ce0091d403c1da1c6fdf2b5ab8c7626d6c7b
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-19 18:42:56 +0300

    Update main.js to call display objects function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 import './style.css';
+
+import printArtObjects from './modules/displayArtObject.js';
+// import getScore from './modules/getItems.js';
+
+// const artsTable = document.querySelector('.arts')
+// const printScore = async () => {
+//     const result = await getScore();
+//     const table = document.createElement('table');
+//     table.innerHTML = '';
+//    result.forEach((res) => {
+//     table.innerHTML += `
+//         <tr><td colspan="2"> <img src="${res.primaryImage}" alt="object image"</td></tr>
+//         <tr><td colspan="2"> ${res.title}</td></tr>`
+//         artsTable.appendChild(table);
+//     });
+// };
+
+//   printScore();
+
+const likeBtn = document.querySelector('.likeBtn');
+
+// call the addScore function and add the score
+// likeBtn.addEventListener('click', hello);
+
+// function hello () {
+//     alert("Thumbus Up!!");
+// }
+printArtObjects();
