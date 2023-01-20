2d7ce0dcf7d248e5029436572b576a09a344d131
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-20 06:45:44 +0300

    Update baseURL and API object's id

--- src/modules/objectIds.js
+++ src/modules/objectIds.js
@@ -1,3 +1,3 @@
-const objectId = [2592, 13938, 188545, 195133, 195138, 251167, 288491, 312572, 451102];
+const objectsId = [2592, 13938, 188545, 195133, 195138, 251167, 288491, 312572, 451102];
 
-export default objectId;
+export default objectsId;
