7053a8a50360f4a9ab86ceefe83ecfce54cb6ae6
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-20 06:44:44 +0300

    rename files (url.js and getArtObject.js)

--- src/modules/getArtObject.js
+++
@@ -1,9 +0,0 @@
-import url from './url.js';
-
-const getArtObjects = async (objectId) => {
-  const response = await fetch(url + objectId);
-  const res = await response.json();
-  return res;
-};
-
-export default getArtObjects;
--- src/modules/url.js
+++
@@ -1,4 +0,0 @@
-const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
-// const url = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
-
-export default url;
