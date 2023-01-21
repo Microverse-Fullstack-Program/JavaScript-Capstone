5042d00fa06d1655cb03d33a689a918c01c3391d
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-22 01:56:27 +0300

    update and refactor baseURL module

--- src/modules/baseURLs.js
+++ src/modules/baseURLs.js
@@ -1,5 +1,8 @@
-const externalApi = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
-const involvementApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/zbJ8UiXamkbOKYu3Pa9I/likes';
-const commentURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/zbJ8UiXamkbOKYu3Pa9I/comments';
+const externalAPI = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/zbJ8UiXamkbOKYu3Pa9I/'
 
-export { externalApi, involvementApi, commentURL };
+const likesURL = baseUrl + 'likes';
+const commentsURL = baseUrl + 'comments';
+const reservationsURL = baseUrl + 'reservations/';
+
+export { externalAPI, likesURL, commentsURL, reservationsURL };
