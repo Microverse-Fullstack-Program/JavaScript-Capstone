0fb0f04a02a38aba2d4923fe92fed6696ccd3524
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-01-21 18:37:19 +0200

    Revert "Popup"

--- src/main.js
+++ src/main.js
@@ -1,11 +1,1 @@
 import './style.css';
-import { window } from './modules/window.js';
-
-const commentBtn = document.querySelectorAll('.CommentsButton');
-
-// Handels all "comments" button popup execution
-commentBtn.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    window(e.target.id);
-  });
-});
--- src/modules/commentPage.test.js
+++
@@ -1,5 +0,0 @@
-// const commentPage = require('./commentPage.js');
-
-// test('counts total comments', () => {
-//   expect(commentPage(null, 6)).toBe(6);
-// });
